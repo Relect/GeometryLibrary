f5f79d3ae1aa4b3eef5fee4c2bfc0eb355fc46c3
Gennadiy <relect@bk.ru>
2024-11-25 17:51:29 +0300

    add square

---
+++ GeometryShape/src/main/java/org/example/Square.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Square implements Shape {
+
+    private final double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return side * 4;
+    }
+}
