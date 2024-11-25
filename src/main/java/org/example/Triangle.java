dbab847746cff58b81fde76a797ec72b99b123c6
Gennadiy <relect@bk.ru>
2024-11-25 10:50:32 +0300

    add

---
+++ src/main/java/org/example/Triangle.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Triangle implements Shape {
+
+    private final double a;
+    private final double b;
+    private final double c;
+
+    public Triangle(double a, double b, double c) throws Exception {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if (a+b < c || a+c < b || b+c < a) {
+            throw new Exception("Triangle does not exist, the sides are incorrectly");
+        }
+    }
+
+    @Override
+    public double getArea() {
+        double p = getPerimeter() / 2;
+        // формула Герона
+        double s = Math.sqrt(p * (p-a)*(p-b)*(p-c));
+        return s;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return a+b+c;
+    }
+}
